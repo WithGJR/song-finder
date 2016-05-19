f36f6cd0a20a795ce4c12a9bc4d12f3437018e37
WithGJR <kevin3372000@gmail.com>
2016-05-18 20:31:49 -0700

    add methods for models/Song.go

--- server/models/Song.go
+++ server/models/Song.go
@@ -6,12 +6,30 @@ import (
 
 type Song struct {
 	gorm.Model
-	Name    string
-	Country string
+	Name     string
+	Country  string
 	Lyricist string
 	Composer string
-    Length string
+	Length   string
 
 	SingerId uint
-	AlbumId uint
+	AlbumId  uint
+}
+
+func GetAllSong(songs *[]Song) error {
+	db, err := connectDB()
+	if err != nil {
+		return err
+	}
+	db.Find(songs)
+	return nil
+}
+
+func (this *Song) Create() error {
+	db, err := connectDB()
+	if err != nil {
+		return err
+	}
+	db.Create(this)
+	return nil
 }
